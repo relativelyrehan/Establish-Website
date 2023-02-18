0b61fbf8b179b7bd57c80d3daa3d55be8bd1c102
relativelyrehan <rehan18alam@gmail.com>
2023-02-18 19:31:05 +0530

    xx

--- components/BorderButton.js
+++ components/BorderButton.js
@@ -1,3 +1,5 @@
+import { Oval } from 'react-loader-spinner';
+
 export const BorderButton = ({ onClick, containerClass, px = "px-6", py = "py-2", title, disabled, fontSize, loading }) => {
     return (
         <button
@@ -5,7 +7,18 @@ export const BorderButton = ({ onClick, containerClass, px = "px-6", py = "py-2"
             disabled={disabled || loading}
             className={`${containerClass} ${fontSize ? fontSize : 'text-base lg:text-xl'} made-gentle border border-white border-opacity-80 rounded-full ${px} ${py} hover:border-sunset ${disabled || loading ? 'cursor-not-allowed' : 'cursor-pointer'}}`}
         >
-            {title}
+            {loading ? <Oval
+                height={16}
+                width={16}
+                color="#fff"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                ariaLabel="oval-loading"
+                secondaryColor="#fff"
+                strokeWidth={6}
+                strokeWidthSecondary={6}
+            /> : title}
         </button>
     )
 }
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,33 +1,31 @@
 import { useRouter } from "next/router";
 import { useContext, useEffect, useState } from "react";
 import { StateContext } from "../utils/context/context";
-import { Input, Layout, Modal, Navbar, Note, Tab } from "../components";
-import { RiTodoLine, RiMastodonLine } from "react-icons/ri";
-import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
-import { deleteData, getData, patchData, putData } from "../utils/service";
+import { Layout, Note, Tab } from "../components";
+import { deleteData, getData } from "../utils/service";
 import { toast, Toaster } from "react-hot-toast";
 import Image from "next/image";
+import { Oval } from "react-loader-spinner";
 export default function Dashboard() {
     const router = useRouter();
-    const [appState, dispatch] = useContext(StateContext);
-    const [create, setCreate] = useState(false);
+    const [appState] = useContext(StateContext);
     const [remove, setRemove] = useState(-1);
-    const [edit, setEdit] = useState(-1);
-    const [editObject, setEditObject] = useState({
-        title: "",
-        content: "",
-    })
     const [selectedTab, setSelectedTab] = useState("all");
     const [allNotes, setAllNotes] = useState([]);
-    const [user, setUser] = useState({});
+    const [loading, setLoading] = useState(false);
 
     const getAllNotes = async (id) => {
         try {
+            setLoading(true);
             const response = await getData(`/posts/getNotes/${id}`);
             if (response?.status == 200) {
+                setLoading(false);
                 setAllNotes(response?.data);
+            } else {
+                setLoading(false);
             }
         } catch (e) {
+            setLoading(false);
             console.log("error", e);
         }
     };
@@ -38,50 +36,81 @@ export default function Dashboard() {
             if (response.status == 200) {
                 getAllNotes(appState?.user?._id);
                 setRemove(-1);
-                toast.success("Note deleted successfully")
+                toast.success("Note deleted successfully");
             }
         } catch (e) {
             console.log("error", e);
         }
     };
 
-    const handleEditNote = async (id) => {
-        try {
-            const response = await putData(`/posts/updateNote/${id}`, {
-                title: editObject.title,
-                content: editObject.content,
-                userId: appState?.user?._id,
-            });
-            if (response.status == 200) {
-                getAllNotes(appState?.user?._id);
-                setEdit(-1);
-            }
-        } catch (e) {
-            console.log("error", e);
-        }
-    };
+    // const handleEditNote = async (id) => {
+    //     try {
+    //         const response = await putData(`/posts/updateNote/${id}`, {
+    //             title: editObject.title,
+    //             content: editObject.content,
+    //             userId: appState?.user?._id,
+    //         });
+    //         if (response.status == 200) {
+    //             getAllNotes(appState?.user?._id);
+    //             setEdit(-1);
+    //         }
+    //     } catch (e) {
+    //         console.log("error", e);
+    //     }
+    // };
 
     useEffect(() => {
         if (appState?.user?._id) {
             getAllNotes(appState?.user?._id);
         }
-    }, [appState])
+    }, [appState]);
 
+    if (loading) {
+        return (
+            <Layout>
+                <div className="mt-8 flex flex-wrap gap-4 justify-center">
+                    <div className="w-1/5 h-60 bg-gray-700 animate-pulse rounded-lg"></div>
+                    <div className="w-1/5 h-60 bg-gray-700 animate-pulse rounded-lg"></div>
+                    <div className="w-1/5 h-60 bg-gray-700 animate-pulse rounded-lg"></div>
+                    <div className="w-1/5 h-60 bg-gray-700 animate-pulse rounded-lg"></div>
+                    <div className="w-1/5 h-60 bg-gray-700 animate-pulse rounded-lg"></div>
+                    <div className="w-1/5 h-60 bg-gray-700 animate-pulse rounded-lg"></div>
+                    <div className="w-1/5 h-60 bg-gray-700 animate-pulse rounded-lg"></div>
+                </div>
+            </Layout>
+        )
+    }
 
     return (
         <Layout>
             <Tab setSelectedTab={setSelectedTab} selectedTab={selectedTab} />
 
-            {allNotes?.length ? <div className="mt-8 grid grid-cols-12 gap-4 text-black">
-                {allNotes?.map((note, key) => {
-                    return (
-                        <Note router={router} key={key} remove={remove} handleDeleteNote={handleDeleteNote} note={note} setRemove={setRemove} />
-                    );
-                })}
-            </div> : <div className="p-10 flex justify-center items-center flex-col">
-                <Image src={'/images/empty.svg'} alt="No Notes" height={300} width={300} />
-                <p className="made-gentle text-2xl text-center mt-4">No Data found</p>
-            </div>}
+            {allNotes?.length ? (
+                <div className="mt-8 grid grid-cols-12 gap-4 text-black">
+                    {allNotes?.map((note, key) => {
+                        return (
+                            <Note
+                                router={router}
+                                key={key}
+                                remove={remove}
+                                handleDeleteNote={handleDeleteNote}
+                                note={note}
+                                setRemove={setRemove}
+                            />
+                        );
+                    })}
+                </div>
+            ) : (
+                <div className="p-10 flex justify-center items-center flex-col">
+                    <Image
+                        src={"/images/empty.svg"}
+                        alt="No Notes"
+                        height={300}
+                        width={300}
+                    />
+                    <p className="made-gentle text-2xl text-center mt-4">No Data found</p>
+                </div>
+            )}
         </Layout>
     );
 }
