e02c4b5c883a23c39dd539378e72927a00b4a714
relativelyrehan <rehan18alam@gmail.com>
2023-03-09 14:15:25 +0530

    xx

---
+++ pages/open.js
@@ -0,0 +1,15 @@
+import { useDebugValue, useEffect } from "react";
+
+export function Open() {
+    useEffect(() => {
+        if (window) {
+            window.location.href = "x-webkit-app-launch://?url=https://www.google.com";
+        }
+    }, [])
+
+    return (
+        <div className="flex h-screen w-full bg-black items-center justify-center">
+            Opening Safari
+        </div>
+    )
+}
