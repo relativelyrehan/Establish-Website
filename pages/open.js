d6fc0d47e9d4b7d4603ca8456e556d7bca65df2a
relativelyrehan <rehan18alam@gmail.com>
2023-03-09 18:55:06 +0530

    x

--- pages/open.js
+++ pages/open.js
@@ -4,15 +4,19 @@ export default function Open() {
     useEffect(() => {
         const url = "https://www.google.com";
         if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
-            window.location.href = "https://maps.apple.com/?address=" + encodeURIComponent(url);
+            const a = document.createElement("a");
+            a.href = url;
+            a.target = "_blank";
+            a.download = "download";
+            a.click();
         } else {
             window.open(url);
         }
-    }, [])
+    }, []);
 
     return (
         <div className="flex h-screen w-full bg-black items-center justify-center">
             Opening Safari
         </div>
-    )
+    );
 }
