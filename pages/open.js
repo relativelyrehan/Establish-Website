a5a7026e97c926bdf1b4820d5e4a02fe5f32535a
relativelyrehan <rehan18alam@gmail.com>
2023-03-13 16:24:36 +0530

    rs

--- pages/open.js
+++ pages/open.js
@@ -2,19 +2,21 @@ import { useEffect } from "react";
 
 export default function Open() {
     useEffect(() => {
-        try {
-            window.location.href = 'jikasdf://track/05Eq6SWneoZYvKQbm2xN25?si=s38D71i_TEyk_RrnhlYIbg&app_destination=copy-link&context=spotify%3Aalbum%3A4sLtOBOzn4s3GDUv3c5oJD';
-        } catch (e) {
-            setTimeout(() => {
+        if (typeof window !== "undefined") {
+            window.location.href =
+                "spotify://track/05Eq6SWneoZYvKQbm2xN25?si=s38D71i_TEyk_RrnhlYIbg&app_destination=copy-link&context=spotify%3Aalbum%3A4sLtOBOzn4s3GDUv3c5oJD";
+            window.onerror = function () {
                 window.location.href = "www.google.com"
-            }, 500)
+                console.error(
+                    "Failed to launch:",
+                );
+            };
         }
     }, []);
 
     return (
         <div className="flex h-screen w-full bg-black items-center justify-center">
             <h1 className="text-white text-4xl">Redirecting...</h1>
-
         </div>
     );
 }
