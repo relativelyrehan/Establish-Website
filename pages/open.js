c04c9a3cdc0cc5ef7f6afb27ff99cfae9fee29f1
relativelyrehan <rehan18alam@gmail.com>
2023-03-13 16:30:47 +0530

    rs

--- pages/open.js
+++ pages/open.js
@@ -3,14 +3,14 @@ import { useEffect } from "react";
 export default function Open() {
     useEffect(() => {
         if (typeof window !== "undefined") {
+            window.addEventListener("beforeunload", function (event) {
+                event.preventDefault();
+                event.returnValue = "";
+                window.location.href = "www.google.com";
+            });
+
             window.location.href =
                 "spotifyooo://track/05Eq6SWneoZYvKQbm2xN25?si=s38D71i_TEyk_RrnhlYIbg&app_destination=copy-link&context=spotify%3Aalbum%3A4sLtOBOzn4s3GDUv3c5oJD";
-            window.onerror = function () {
-                window.location.href = "www.google.com"
-                console.error(
-                    "Failed to launch:",
-                );
-            };
         }
     }, []);
 
