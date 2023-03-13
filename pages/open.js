e30e521a5fa49de535238bdeec0ee6957573d9f6
relativelyrehan <rehan18alam@gmail.com>
2023-03-13 16:15:55 +0530

    rs

--- pages/open.js
+++ pages/open.js
@@ -3,12 +3,7 @@ import { useEffect } from "react";
 export default function Open() {
     useEffect(() => {
         try {
-            const url = "https://www.google.com";
-            if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
-                window.location.href = 'jikasdf://track/05Eq6SWneoZYvKQbm2xN25?si=s38D71i_TEyk_RrnhlYIbg&app_destination=copy-link&context=spotify%3Aalbum%3A4sLtOBOzn4s3GDUv3c5oJD';
-            } else {
-                window.open(url);
-            }
+            window.location.href = 'jikasdf://track/05Eq6SWneoZYvKQbm2xN25?si=s38D71i_TEyk_RrnhlYIbg&app_destination=copy-link&context=spotify%3Aalbum%3A4sLtOBOzn4s3GDUv3c5oJD';
         } catch (e) {
             setTimeout(() => {
                 window.location.href = "www.google.com"
