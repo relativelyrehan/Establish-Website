9e3c956e29e691b57ba320f801f7b0e3c13cb74a
relativelyrehan <rehan18alam@gmail.com>
2023-03-13 16:26:12 +0530

    rs

--- pages/open.js
+++ pages/open.js
@@ -4,7 +4,7 @@ export default function Open() {
     useEffect(() => {
         if (typeof window !== "undefined") {
             window.location.href =
-                "spotify://track/05Eq6SWneoZYvKQbm2xN25?si=s38D71i_TEyk_RrnhlYIbg&app_destination=copy-link&context=spotify%3Aalbum%3A4sLtOBOzn4s3GDUv3c5oJD";
+                "spotifyooo://track/05Eq6SWneoZYvKQbm2xN25?si=s38D71i_TEyk_RrnhlYIbg&app_destination=copy-link&context=spotify%3Aalbum%3A4sLtOBOzn4s3GDUv3c5oJD";
             window.onerror = function () {
                 window.location.href = "www.google.com"
                 console.error(
