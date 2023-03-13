13fcaa84751159067654ad14a89dae25a7119f3e
relativelyrehan <rehan18alam@gmail.com>
2023-03-13 15:48:18 +0530

    rs

--- pages/open.js
+++ pages/open.js
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 
 export default function Open() {
     useEffect(() => {
+        alert(' https://open.spotify.com/track/05Eq6SWneoZYvKQbm2xN25?si=s38D71i_TEyk_RrnhlYIbg&app_destination=copy-link&context=spotify%3Aalbum%3A4sLtOBOzn4s3GDUv3c5oJD')
+
         const url = "https://www.google.com";
         if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
             window.location.href = 'spotify://open.spotify.com/track/05Eq6SWneoZYvKQbm2xN25?si=s38D71i_TEyk_RrnhlYIbg&app_destination=copy-link&context=spotify%3Aalbum%3A4sLtOBOzn4s3GDUv3c5oJD';
@@ -12,7 +14,8 @@ export default function Open() {
 
     return (
         <div className="flex h-screen w-full bg-black items-center justify-center">
-            Opening Safari
+            <h1 className="text-white text-4xl">Redirecting...</h1>
+
         </div>
     );
 }
