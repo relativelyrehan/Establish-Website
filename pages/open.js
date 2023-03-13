6622b6952ef3be4130b8185b31c7a87a2d22b505
relativelyrehan <rehan18alam@gmail.com>
2023-03-13 16:10:56 +0530

    rs

--- pages/open.js
+++ pages/open.js
@@ -2,13 +2,17 @@ import { useEffect } from "react";
 
 export default function Open() {
     useEffect(() => {
-        alert(' https://open.spotify.com/track/05Eq6SWneoZYvKQbm2xN25?si=s38D71i_TEyk_RrnhlYIbg&app_destination=copy-link&context=spotify%3Aalbum%3A4sLtOBOzn4s3GDUv3c5oJD')
-
-        const url = "https://www.google.com";
-        if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
-            window.location.href = 'spotify://track/05Eq6SWneoZYvKQbm2xN25?si=s38D71i_TEyk_RrnhlYIbg&app_destination=copy-link&context=spotify%3Aalbum%3A4sLtOBOzn4s3GDUv3c5oJD';
-        } else {
-            window.open(url);
+        try {
+            const url = "https://www.google.com";
+            if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
+                window.location.href = 'jikasdf://track/05Eq6SWneoZYvKQbm2xN25?si=s38D71i_TEyk_RrnhlYIbg&app_destination=copy-link&context=spotify%3Aalbum%3A4sLtOBOzn4s3GDUv3c5oJD';
+            } else {
+                window.open(url);
+            }
+        } catch (e) {
+            setTimeout(() => {
+                window.location.href = "www.google.com"
+            }, 500)
         }
     }, []);
 
