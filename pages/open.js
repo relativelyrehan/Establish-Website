4d832a78ddf6dd21f27928249293f716e694db00
relativelyrehan <rehan18alam@gmail.com>
2023-04-20 18:37:24 +0530

    test seo

--- pages/open.js
+++ pages/open.js
@@ -1,22 +1,70 @@
-import { useEffect } from "react";
+import { NextSeo } from "next-seo";
+import { useEffect, useState } from "react";
 
 export default function Open() {
+
+    const [seoData, setSeoData] = useState({
+        ogTitle: "LinkPilot | Smart Link",
+        ogDescription: "Links that Directly Open Apps & Earn you money",
+        ogImage: "https://linkpilot.app/og-image.png",
+    });
+
     useEffect(() => {
-        if (typeof window !== "undefined") {
-            window.addEventListener('beforeunload', function (e) {
-                window.addEventListener("error", (event) => {
-                    window.location.href = "www.google.com"
-                });
-            });
-            // youtube.com/@thedaddork
-            window.location.href =
-                "vnd.youtube://youtube.com/@thedaddork";
-        }
+        (async function getSEOData() {
+            const res = await getData('https://redirection.linkpilot.app/api/v1/links/seo-data?slug=dqnz');
+            if (res.status === 200) {
+                console.log(res.data.data)
+                setSeoData({
+                    ogTitle: res?.data?.data?.oGraph?.ogTitle || "LinkPilot | Smart Link",
+                    ogDescription: res?.data?.data?.oGraph?.ogDescription || "Links that Directly Open Apps & Earn you money",
+                    ogImage: res?.data?.data?.oGraph?.ogImage?.url || "https://linkpilot.app/og-image.png",
+                })
+            }
+        })();
     }, []);
 
+    // useEffect(() => {
+    //     if (typeof window !== "undefined") {
+    //         window.addEventListener('beforeunload', function (e) {
+    //             window.addEventListener("error", (event) => {
+    //                 window.location.href = "www.google.com"
+    //             });
+    //         });
+    //         // youtube.com/@thedaddork
+    //         window.location.href =
+    //             "vnd.youtube://youtube.com/@thedaddork";
+    //     }
+    // }, []);
+
     return (
-        <div className="flex h-screen w-full bg-black items-center justify-center">
-            <h1 className="text-white text-4xl">Redirecting...</h1>
-        </div>
+        <>
+            <NextSeo
+                title={seoData?.ogTitle}
+                description={seoData?.ogDescription}
+                openGraph={{
+                    type: 'website',
+                    url: 'https://linkpilot.me/',
+                    title: seoData?.ogTitle,
+                    description: 'Links that Directly Open Apps & Earn you money',
+                    images: [
+                        {
+                            url: seoData?.ogImage,
+                            width: 800,
+                            height: 600,
+                            alt: 'LinkPilot | Smart Link',
+                        },
+                        {
+                            url: seoData?.ogImage,
+                            width: 800,
+                            height: 600,
+                            alt: 'LinkPilot | Smart Link',
+                        },
+                    ],
+                }}
+            />
+            <div className="flex h-screen w-full bg-black items-center justify-center">
+                <h1 className="text-white text-4xl">Redirecting...</h1>
+            </div>
+        </>
     );
 }
