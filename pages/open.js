3284a9f1bf8b4c80efd98e77875b002bc9b8bbb8
relativelyrehan <rehan18alam@gmail.com>
2023-04-20 11:00:17 +0530

    xx

--- pages/open.js
+++ pages/open.js
@@ -3,11 +3,13 @@ import { useEffect } from "react";
 export default function Open() {
     useEffect(() => {
         if (typeof window !== "undefined") {
-            window.addEventListener("error", (event) => {
-                window.location.href = "www.google.com"
+            window.addEventListener('beforeunload', function (e) {
+                window.addEventListener("error", (event) => {
+                    window.location.href = "www.google.com"
+                });
             });
             window.location.href =
-                "spotifyooo://track/05Eq6SWneoZYvKQbm2xN25?si=s38D71i_TEyk_RrnhlYIbg&app_destination=copy-link&context=spotify%3Aalbum%3A4sLtOBOzn4s3GDUv3c5oJD";
+                "vnd.youtube://@TalkingThrones";
         }
     }, []);
 
